ee4f6e03f72f7ee09a9b2efb8d7a57b3c484aa9e
Arif Bipu <arifulislambipubd.com>
2022-10-01 17:49:58 +0600

    felte implementation

--- src/routes/admin/trainers/Form.svelte
+++ src/routes/admin/trainers/Form.svelte
@@ -9,11 +9,10 @@
 		Button
 	} from 'carbon-components-svelte';
 	import * as yup from 'yup';
-	import { createForm } from 'svelte-forms-lib';
+	import { validator } from '@felte/validator-yup';
+	import { createForm } from 'felte';
 	import { createEventDispatcher } from 'svelte';
-	import { onMount } from 'svelte';
 	type FormType = 'edit' | 'create';
-
 	const dispatch = createEventDispatcher();
 
 	export let formType: FormType;
@@ -28,60 +27,38 @@
 		remarks: null
 	};
 
-	let trainer = {
-		name: null,
-		organization: null,
-		gender: null,
-		venue: null,
-		date: null,
-		duration: 4,
-		subject: null,
-		remarks: null
-	};
+	const schema = yup.object({
+		name: yup.string().required(),
+		oraganizations: yup.string().required()
+	});
+
+	const { form, errors } = createForm({
+		initialValues: initialData,
+		onSubmit: (values) => {
+			console.log(values);
+		},
+		extend: validator({ schema })
+	});
 
 	let ButtonName = {
 		edit: 'Edit',
 		Add: 'Add'
 	};
-
-	$: if (formType == 'edit') {
-		trainer = initialData;
-		$form = initialData;
-	}
-
-	const { form, errors, handleSubmit } = createForm({
-		initialValues: trainer,
-		validationSchema: yup.object().shape({
-			name: yup.string().required(),
-			organization: yup.string().required(),
-			gender: yup.string().oneOf(['Male', 'Female']).required(),
-			venue: yup.string().required(),
-			date: yup.date().required(),
-			duration: yup.number().required(),
-			subject: yup.string().required(),
-			remarks: yup.string().required()
-		}),
-		onSubmit: (values) => {
-			console.log(values);
-			if (formType == 'edit') {
-				dispatch('edit', trainer);
-			} else if (formType == 'create') {
-				dispatch('create', trainer);
-			}
-		}
-	});
 </script>
 
-<form on:submit={handleSubmit}>
+<form use:form>
 	<div class="t-grid t-grid-cols-4 t-gap-4">
 		<div>
-			<TextInput bind:value={$form.name} labelText="User name" placeholder="Enter user name..." />
-			{#if $errors.name}
-				<small>{$errors.name}</small>
-			{/if}
+			<TextInput
+				name="name"
+				labelText="User name"
+				placeholder="Enter user name..."
+				invalid={$errors.name != null}
+				bind:invalidText={$errors.name}
+			/>
 		</div>
 		<div>
-			<Select bind:selected={$form.organization} labelText="Organization">
+			<Select name="organizations" labelText="Organization">
 				<SelectItem value="a" disabled hidden />
 				<SelectItem value="b" />
 				<SelectItem value="c" />
@@ -90,37 +67,35 @@
 			</Select>
 		</div>
 		<div>
-			<Select bind:selected={$form.gender} labelText="Gender">
+			<Select name="gender" labelText="Gender">
 				<SelectItem value="Male" />
 				<SelectItem value="Female" />
 			</Select>
 		</div>
 		<div>
-			<Select bind:selected={$form.venue} labelText="Venue">
+			<Select name="venue" labelText="Venue">
 				<SelectItem value="Dhaka" />
 				<SelectItem value="Noakhali" />
 			</Select>
 		</div>
 		<div>
-			<DatePicker bind:value={$form.date} datePickerType="single">
+			<DatePicker name="date" datePickerType="single">
 				<DatePickerInput labelText="Meeting date" placeholder="mm/dd/yyyy" />
 			</DatePicker>
 		</div>
 		<div>
 			<NumberInput
-				bind:value={$form.duration}
-				min={0}
-				max={20}
+				name="duration"
 				invalidText="Number must be between 4 and 20."
 				helperText="Clusters provisioned in your region"
 				label="Clusters (4 min, 20 max)"
 			/>
 		</div>
 		<div>
-			<TextInput bind:value={$form.subject} labelText="Subject" placeholder="Enter Subject..." />
+			<TextInput name="subject" labelText="Subject" placeholder="Enter Subject..." />
 		</div>
 		<div>
-			<TextInput bind:value={$form.remarks} labelText="Remarks" placeholder="Remarks..." />
+			<TextInput name="remarks" labelText="Remarks" placeholder="Remarks..." />
 		</div>
 		<div>
 			<Button type="submit">
