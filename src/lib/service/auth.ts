ce48f79647d44aa14b1fc04759ee73495cd9e916
arifulislam1 <arifulislambipubd.com>
2023-01-17 18:33:30 +0600

    deployed api connection

--- src/lib/service/auth.ts
+++ src/lib/service/auth.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const http = axios.create({ baseURL: 'http://127.0.0.1:8000/api/' });
+export const http = axios.create({ baseURL: 'http://3.137.0.157:8000/api/' });
 
 http.interceptors.response.use(
 	(res) => res,
@@ -25,7 +25,7 @@ http.interceptors.response.use(
 				}
 			}
 			if (err.response.status === 403 && err.response.data) {
-				return Promise.reject(err.response.data)
+				return Promise.reject(err.response.data);
 			}
 		}
 		return Promise.reject(err);
@@ -53,7 +53,7 @@ export function logout() {
 export async function refresh() {
 	const _refresh = localStorage.getItem('refreshToken');
 	try {
-		const resp = await axios.post('http://127.0.0.1:8000/api/token/refresh/', {
+		const resp = await axios.post('http://3.137.0.157:8000/api/token/refresh/', {
 			refresh: _refresh
 		});
 		return resp;
@@ -68,7 +68,7 @@ export async function login(username, password) {
 	try {
 		const {
 			data: { access, user, refresh }
-		} = await axios.post('http://127.0.0.1:8000/api/token/', { username, password });
+		} = await axios.post('http://3.137.0.157:8000/api/token/', { username, password });
 		localStorage.setItem('accessToken', access);
 		localStorage.setItem('refreshToken', refresh);
 
