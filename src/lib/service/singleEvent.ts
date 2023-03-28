f75fb7ad8d70522a28981604e7f933cdce5ddb8c
Arifbipubd <arifulislambipubd@gmail.com>
2023-03-28 23:45:04 +0600

    dashboard chart api integration

---
+++ src/lib/service/singleEvent.ts
@@ -0,0 +1,31 @@
+import { http } from '$lib/service/auth';
+
+export async function getSingleEvent(
+	eventId,
+	organization = null,
+	year = null,
+	division = null,
+	district = null,
+	subDistrict = null
+) {
+	try {
+		const { data } = await http.get(`event/${eventId}/events/`, {
+			params: {
+				organization,
+				year,
+				division,
+				district,
+				sub_district: subDistrict
+			}
+		});
+		return {
+			status: 200,
+			data
+		};
+	} catch (err) {
+		return Promise.resolve({
+			status: 400,
+			data: []
+		});
+	}
+}
